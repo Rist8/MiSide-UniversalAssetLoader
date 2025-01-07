41f7468af7e0db0bf589337d183a2ab7f1bc3063
Rist8 <94626383+Rist8@users.noreply.github.com>
2025-01-07 16:17:48 +0100

    Update PluginLoader.cs
    Finally corrected plugin's version

--- Plugin/PluginLoader.cs
+++ Plugin/PluginLoader.cs
@@ -7,13 +7,13 @@ public static class PluginInfo
 {
 	public const string PLUGIN_GUID = "UniversalAssetLoader";
 	public const string PLUGIN_NAME = "Universal Asset Loader";
-	public const string PLUGIN_VERSION = "1.1.0";
+	public const string PLUGIN_VERSION = "0.10.0";
 
 	public static PluginLoader Instance;
 	public static string AssetsFolder = Paths.PluginPath + "\\" + PluginInfo.PLUGIN_GUID + "\\Assets";
 }
 
-[BepInPlugin("org.miside.plugins.assetloader", PluginInfo.PLUGIN_NAME, "1.1.0")]
+[BepInPlugin("org.miside.plugins.assetloader", PluginInfo.PLUGIN_NAME, "0.10.0")]
 public class PluginLoader : BasePlugin
 {
 	public ManualLogSource Logger { get; private set; }
